2657595fa41983179234b34d74cfcb35c5549c5a
vodkanlime <167576062+vodkanlime@users.noreply.github.com>
2024-04-20 01:04:40 +0530

    Add files via upload

---
+++ wedding.js
@@ -0,0 +1,8 @@
+function showForm() {
+    var form = document.getElementById('detailsForm');
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+}
